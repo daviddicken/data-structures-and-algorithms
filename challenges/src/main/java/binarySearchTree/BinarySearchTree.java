7f694d6c9773281f74de7d5e14fbf6a1b420a168
daviddicken <davidfromseattle@gmail.com>
2020-10-03 19:52:53 -0700

    added some notes, finished up code challenge, and completed read me

--- challenges/src/main/java/binarySearchTree/BinarySearchTree.java
+++ challenges/src/main/java/binarySearchTree/BinarySearchTree.java
@@ -1,6 +1,9 @@
 package binarySearchTree;
 
+import java.util.ArrayList;
+
 class BinarySearchTree {
+    ArrayList<Integer> arrBuild = new ArrayList<>();
     private Node root;
 
     //====== Constructor ===============
@@ -8,17 +11,44 @@ class BinarySearchTree {
         this.root = null;
     }
 
+    //=========== in order =========================
+    public ArrayList inOrder(){
+
+        arrBuild = new ArrayList<>(); // Reset ArrayList everytime inOrder is called
+        return inOrder(root);         // Pass root to inOrder helper method
+    }
+    //----------- Helper --------------------------
+    private ArrayList inOrder(Node node) {
+
+        if (node == null) {return arrBuild;} // if tree is empty return an empty array list
+
+        inOrder(node.getLeftChild());  // Recursively traverse through tree
+        arrBuild.add(node.getValue()); // adding the left child values then the right child values to the array list
+        inOrder(node.getRightChild());
+
+        return arrBuild;
+    }
+
+    //======== Contains =================
+    public boolean contains (int value) throws Exception {
+        if(root == null){
+            throw new Exception("This tree is empty"); // if tree empty throw exception
+        }
+        return inOrder(root).contains(value); // call in order to get an array list of tree. Then check if array contains
+    }                                         // search value
+
     //https://www2.hawaii.edu/~esb/2011fall.ics211/BinarySearchTree.java.html
     //======== Add Node =================
     public void addNode(int value) {
-        root = addNode(root, value);
+        root = addNode(root, value); //send in root to recursively traverse through tree
     }
     //------------- helper -------------
     private Node addNode(Node current, int value){
+        // Base case
         if (current == null){
-            return new Node(value);
+            return new Node(value); // if empty child is reached set child to new node
         }
-
+            // if new node value is less the current node value recursively traverse to the left child if greater go right
         if(current.getValue() > value){
           current.setLeftChild(addNode(current.getLeftChild(), value));
         }else{
--- challenges/src/test/java/binarySearchTree/binarySearchTreeTest.java
+++ challenges/src/test/java/binarySearchTree/binarySearchTreeTest.java
@@ -5,45 +5,71 @@ import static org.junit.Assert.*;
 
 public class binarySearchTreeTest {
     @Test
-            public void searchTreeTest() {
+            public void searchTreeTest() throws Exception {
+
+        // Can successfully instantiate an empty tree
         BinarySearchTree testTree = new BinarySearchTree();
+        assertEquals("", testTree.toString());
 
+        // Can successfully instantiate a tree with a single root node
         testTree.addNode( 10);
+        assertEquals(10, testTree.getRoot().getValue());
+
+        // Successfully place a larger value in the right child
         testTree.addNode( 20);
+        assertEquals(20, testTree.getRoot().getRightChild().getValue());
+
+        //Successfully place smaller value in left node
         testTree.addNode( 6);
-        testTree.addNode( 8);
+        assertEquals(6, testTree.getRoot().getLeftChild().getValue());
 
-        assertEquals("6 8 10 20 ", testTree.toString());
+        //again
+        testTree.addNode(4);
+        assertEquals(4, testTree.getRoot().getLeftChild().getLeftChild().getValue());
 
+        //add some more
         testTree.addNode(30);
         testTree.addNode(2);
+        testTree.addNode(15);
+        testTree.addNode(25);
+        testTree.addNode(12);
 
-        assertEquals("2 6 8 10 20 30 ", testTree.toString());
+        // Get the collection
+        assertEquals("[2, 4, 6, 10, 12, 15, 20, 25, 30]", testTree.inOrder().toString());
 
+        //test contains
+        assertTrue(testTree.contains(30));
+        assertTrue(testTree.contains(2));
+        assertTrue(testTree.contains(12));
+        assertFalse(testTree.contains(0));
+        assertFalse(testTree.contains(35));
+        assertFalse(testTree.contains(8));
     }
 
     @Test
     public void binaryTreeTest(){
+
+        // Can successfully instantiate an empty tree
         Tree testTree = new Tree();
-        Tree emptyTree = new Tree();
+        assertEquals("[]", testTree.inOrder().toString());
 
-        testTree.addBinaryTreeNode(testTree.getRoot(), 1);
-        testTree.addBinaryTreeNode(testTree.getRoot(), 2);
-        testTree.addBinaryTreeNode(testTree.getRoot(), 3);
-        testTree.addBinaryTreeNode(testTree.getRoot(), 4);
-        testTree.addBinaryTreeNode(testTree.getRoot(), 5);
-        testTree.addBinaryTreeNode(testTree.getRoot(), 6);
+        // Can successfully instantiate a tree with a single root node
+        testTree.addBinaryTreeNode( 1);
+        assertEquals(1, testTree.getRoot().getValue());
 
-        //in order test
+        // Can successfully add a left child and right child to a single root node
+        testTree.addBinaryTreeNode(2);
+        testTree.addBinaryTreeNode(3);
+        assertEquals(2, testTree.getRoot().getLeftChild().getValue());
+        assertEquals(3, testTree.getRoot().getRightChild().getValue());
+
+        // Can successfully return a collection from a preorder, inorder, and postorder traversal's
+        testTree.addBinaryTreeNode(4);
+        testTree.addBinaryTreeNode(5);
+        testTree.addBinaryTreeNode(6);
+        assertEquals("[1, 2, 4, 5, 3, 6]", testTree.preOrder().toString());
         assertEquals("[4, 2, 5, 1, 6, 3]", testTree.inOrder().toString());
-        testTree.addBinaryTreeNode(testTree.getRoot(), 7);
-        assertEquals("[4, 2, 5, 1, 6, 3, 7]", testTree.inOrder().toString());
-
-        //assertEquals("[]", emptyTree.inOrder().toString());
-
-        System.out.println("PreOrder: " + testTree.preOrder().toString());
-        System.out.println("InOrder: " + testTree.inOrder().toString());
-        System.out.println("PostOrder: " + testTree.postOrder().toString());
+        assertEquals("[4, 5, 2, 6, 3, 1]", testTree.postOrder().toString());
 
     }
 }
