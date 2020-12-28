f28aa29379e396c4660e32147e70e69b5496862f
daviddicken <davidfromseattle@gmail.com>
2020-12-28 00:40:26 -0800

    finished calculator challenge

---
+++ challenges/src/main/java/codewars/Calculator.java
@@ -0,0 +1,32 @@
+package codewars;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Calculator {
+    public static Double evaluate(String expression) {
+        ArrayList<String> arr = new ArrayList<>(Arrays.asList(expression.split(" ")));
+        for(int i = 0; i < arr.size(); i++){
+            if(arr.get(i).equals("/")){
+                arr.set(i+1, String.valueOf(Double.parseDouble(arr.get(i-1)) / Double.parseDouble(arr.get(i+1))));
+                arr.remove(i-1);
+                arr.remove(i-1);
+                --i;
+            }
+            if(arr.get(i).equals("*")){
+                arr.set(i+1, String.valueOf(Double.parseDouble(arr.get(i-1)) * Double.parseDouble(arr.get(i+1))));
+                arr.remove(i-1);
+                arr.remove(i-1);
+                --i;
+            }
+        }
+        double total = Double.parseDouble(arr.get(0));
+
+        for(int i = 1; i < arr.size(); i++){
+            if(arr.get(i).equals("+")) total += Double.parseDouble(arr.get(i+1));
+            if(arr.get(i).equals("-")) total -= Double.parseDouble(arr.get(i+1));
+            i++;
+        }
+        return total;
+    }
+}
