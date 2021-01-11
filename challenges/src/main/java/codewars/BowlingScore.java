7eaf504787d716b6f9e6403cbaa257e1ece5d90c
daviddicken <davidfromseattle@gmail.com>
2021-01-11 01:05:38 -0800

    working on bowling challenge

--- challenges/src/main/java/codewars/BowlingScore.java
+++ challenges/src/main/java/codewars/BowlingScore.java
@@ -11,31 +11,59 @@ public class BowlingScore {
     // if / is found add first value of next frame to frame
     public static int bowling_score(String frames) {
         ArrayList<String> arr = new ArrayList<>(Arrays.asList(frames.split(" ")));
-
-
-        for(int i = 0; i < arr.size(); i++){ // length -1 for 10th frame?
+        int score = 0;
+//"11 11 11 11 11 11 11 11 11 11"
+//"X X X X X X X X X XXX"   size 10    8
+        for(int i = 0; i < arr.size() - 2; i++){
             if(arr.get(i).equals("X")){
-                int total = 10;
-
                 if(arr.get(i+1).equals("X")){
-                    total = 20;
-                    //if(arr.get(i))//check next frame for x
+                    if(arr.get(i + 2).charAt(0) == 'X'){
+                        score += 30;
+                    }else{
+                        score += 20 + arr.get(i+2).charAt(0);
+                    }
                 }else if(arr.get(i+1).contains("/")){
-                    arr.set(i, "20");
-                    break;
+                    score += 20;
                 } else{
-                    total += Integer.parseInt(String.valueOf(arr.get(i+1).charAt(0)));
+                   score += 10 + arr.get(i+1).charAt(0) + arr.get(i+1).charAt(1);
                 }
-
             }else if(arr.get(i).contains("/")){
-
+                if(arr.get(i+1) == "X"){
+                    score += 20;
+                }else{
+                    score += 10 + arr.get(i+1).charAt(0);
+                }
+            }else{
+                score += arr.get(i).charAt(0) + arr.get(i).charAt(1);
             }
-
         }
+//ninth frame
+        if(arr.get(8).equals("X")){
+            if (arr.get(9).charAt(0) == 'X'){
+                if(arr.get(9).charAt(1) == 'X'){
+                    score += 30;
+                }else {
+                    score += 20 + arr.get(9).charAt(1);
+                }
+            }else if(arr.get(9).charAt(1) == '/'){
+                score += 20;
+            }else{
+                score += 10 + arr.get(9).charAt(0) + arr.get(9).charAt(1);
+            }
+        }else if(arr.get(8).contains("/")){
+            if(arr.get(9).charAt(0) == 'X'){
+                score += 20;
+            }else{
+                score += 10 + arr.get(9).charAt(0);
+            }
+        }else{
+            score += arr.get(8).charAt(0) + arr.get(8).charAt(1);
+        }
+//10th frame
+        
 
 
-
-        return 0;
+        return score;
     }
 }
-
+//total += Integer.parseInt(String.valueOf(arr.get(i+1).charAt(0)));
