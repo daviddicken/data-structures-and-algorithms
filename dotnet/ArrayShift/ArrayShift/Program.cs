d41fe2b37f565587077919052894e39224aac7b0
daviddicken <davidfromseattle@gmail.com>
2021-01-12 14:54:32 -0800

    finished code challenge

--- dotnet/ArrayShift/ArrayShift/Program.cs
+++ dotnet/ArrayShift/ArrayShift/Program.cs
@@ -6,7 +6,36 @@ namespace ArrayShift
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] test = new int[5] { 1, 2, 4, 5, 7};
+            int[] arr = ArrayShift(test, 3);
+
+            foreach(int num in arr)
+            {
+                Console.WriteLine(num);
+            }
         }
+
+        public static int[] ArrayShift(int[] arr, int value)
+        {
+            int[] returnArr = new int[arr.Length + 1];
+            int middle = returnArr.Length / 2, j = 0;
+            
+            for (int n = 0; n < returnArr.Length; n++)
+            {
+                if (n == middle)
+                {
+                    returnArr[n] = value;
+                    j--;
+                }
+                else
+                {
+                    returnArr[n] = arr[j];
+                }
+                j++;
+            }
+            return returnArr;
+        }
+
+
     }
 }
