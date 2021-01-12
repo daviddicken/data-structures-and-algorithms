b51f20ba76f7e7be3da2cede3146d289855c36fc
daviddicken <davidfromseattle@gmail.com>
2021-01-11 17:53:15 -0800

    completed reverse array challenge

--- dotnet/ArrayReverse/ArrayReverse/Program.cs
+++ dotnet/ArrayReverse/ArrayReverse/Program.cs
@@ -6,7 +6,22 @@ namespace ArrayReverse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] test = ReverseArray(new int[] { 1, 2, 3, 4, 5 });
+            foreach (int num in test) Console.Write($"{num}, ");
+        }
+
+        static int[] ReverseArray(int [] input)
+        {
+            int[] returnArr = input;
+            int temp = 0, size = returnArr.Length - 1;
+
+            for (int i = 0; i < returnArr.Length / 2; i++)
+            {
+                temp = returnArr[i];
+                returnArr[i] = returnArr[size - i];
+                returnArr[size - i] = temp;
+            }
+            return returnArr;
         }
     }
 }
