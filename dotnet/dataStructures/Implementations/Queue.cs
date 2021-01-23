69dfd362b546de02ae7403c6061666e42ac88407
daviddicken <davidfromseattle@gmail.com>
2021-01-22 23:20:38 -0800

    finished test for stacks and queues

--- dotnet/dataStructures/Implementations/Queue.cs
+++ dotnet/dataStructures/Implementations/Queue.cs
@@ -2,9 +2,95 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Implementations
+namespace DataStructures
 {
-    class Queue
+    public class Queue<T>
     {
+        public Node<T> Front { get; set; }
+        public Node<T> Rear { get; set; }
+
+        public Queue()
+        {
+            Front = null;
+            Rear = null;
+        }
+
+        /// <summary>
+        /// EnQueue takes in a generic value and creates a new node to hold that value. It then places it at the end of the Queuepointing the new nodes next to the rear and then reasigning the newnode to be the rear 
+        /// </summary>
+        /// <param name="val">takes in a generic value</param>
+        public void EnQueue(T val)
+        {
+            Node<T> node = new Node<T>(val);
+
+            if(Front == null)
+            {
+                Front = node;
+                Rear = node; 
+            }
+            else
+            {
+                node.Next = Rear;
+                Rear = node;
+            }
+        }
+
+        /// <summary>
+        /// DeQueue removes the node from the front of the Q and returns the value that was found in that node
+        /// </summary>
+        /// <returns>A generic value</returns>
+        public T DeQueue()
+        {
+            if (Front == null) throw new NullReferenceException("The Queue is empty.");
+
+            T returnVal = Front.Value;
+
+            // if only one node was in the list
+            if(Front == Rear)
+            {
+                Front = null;
+                Rear = null;
+                return returnVal;
+            }
+    
+            Node<T> currentNode = Rear.Next;
+            Node<T> lastNode = Rear;
+
+            while(currentNode.Next != null)
+            {
+                lastNode = currentNode;
+                currentNode = currentNode.Next;
+            }
+
+            // If only two nodes were present in the list 
+            if (lastNode == Rear) 
+            {
+                Front = lastNode;
+                Rear = lastNode;
+                Front.Next = null;
+                return returnVal;
+            }
+
+            lastNode.Next = null;
+            Front = lastNode;
+            return returnVal;
+        }
+
+        /// <summary>
+        /// Peek returns the value found in the front of the Q without modifying the Q
+        /// </summary>
+        /// <returns>generic value</returns>
+        public T Peek()
+        {
+            if (Front == null) throw new NullReferenceException("The Queue is empty.");
+            return Front.Value;
+        }
+
+        /// <summary>
+        /// IsEmpty checks if the front node is null and returns a bool
+        /// </summary>
+        /// <returns>Boolean</returns>
+        public bool IsEmpty() => Front == null;
+
     }
 }
--- dotnet/dataStructures/Implementations/Stack.cs
+++ dotnet/dataStructures/Implementations/Stack.cs
@@ -6,13 +6,12 @@ namespace DataStructures
 {
     public class Stack<T>
     {
-        public Node<T> top;
-
+        public Node<T> Top { get; set; } 
 
         //Constructor
         public Stack()
         {
-            top = null;
+            Top = null;
         }
 
         /// <summary>
@@ -22,8 +21,8 @@ namespace DataStructures
        public void Push(T value)
         {
             Node<T> node = new Node<T>(value);
-            node.Next = top;
-            top = node;
+            node.Next = Top;
+            Top = node;
         }
 
         /// <summary>
@@ -32,7 +31,8 @@ namespace DataStructures
         /// <returns>What ever value type is held in the top node</returns>
         public T Peek()
         {
-            return top.Value;
+            if (Top == null) throw new NullReferenceException("The Stack is empty.");
+            return Top.Value;
         }
         /// <summary>
         /// Pop returns the value found in the top node and removes that node from the stack
@@ -40,21 +40,17 @@ namespace DataStructures
         /// <returns>The value found in the top node</returns>
         public T Pop()
         {
-            T returnVal = top.Value;
-            top = top.Next;
+            if (Top == null) throw new NullReferenceException("The Stack is empty.");
+            T returnVal = Top.Value;
+            Top = Top.Next;
             return returnVal;
         }
 
-/// <summary>
-/// IsEmpty checks if the top node on the stack is null. If it is it returns true otherwise it returns false
-/// </summary>
-/// <returns>boolean</returns>
-        public bool IsEmpty()
-        {
-            if (top == null) return true;
-            return false;
-        }
-
-
+        /// <summary>
+        /// IsEmpty checks if the top node on the stack is null. If it is it returns true otherwise it returns false
+        /// </summary>
+        /// <returns>boolean</returns>
+        public bool IsEmpty() => Top == null;
+        
     }
 }
