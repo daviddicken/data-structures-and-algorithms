281828be49db376d22c69b4606a5345ac2308364
daviddicken <davidfromseattle@gmail.com>
2021-01-23 11:23:19 -0800

    finished piglatin challenge

---
+++ dotnet/SimplePigLatin/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SimplePigLatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PigIt("Pig latin is cool"));
+        }
+
+        public static string PigIt(string str)
+        {
+            string[] strArr = str.Split(' ');
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                string word = strArr[i];
+                if (!Char.IsPunctuation(word[0]))
+                {
+                    word = $"{word.Substring(1)}{word[0]}ay";
+                    Console.WriteLine(word);
+                }
+                strArr[i] = word;
+            }
+
+            return String.Join(" ", strArr);
+        }
+    }
+}
