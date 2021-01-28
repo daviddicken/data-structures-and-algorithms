76af9c709a72bdc93054b394d06b677bd10f7d7f
daviddicken <davidfromseattle@gmail.com>
2021-01-27 22:19:42 -0800

    completed multiBracketValidation

---
+++ dotnet/MultiBracketValidation/MultiBracket.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using DataStructures;
+
+namespace MultiBracketValidation
+{
+    public class MultiBracket
+    {
+
+        public static bool MultiBracketValidation(string input)
+        {
+            if (input == "") throw new NullReferenceException("The input string is empty.");
+            Stack<char> stack = new Stack<char>();
+
+            foreach(char bracket in input)
+            {
+                if(bracket == '(' || bracket == '{' || bracket == '[')
+                {
+                    stack.Push(bracket);
+                }
+                else
+                {
+                    switch (bracket)
+                    {
+                        case ')':
+                            if (stack.IsEmpty() || stack.Peek() != '(') return false;
+                            stack.Pop();
+                            break;
+                        case '}':
+                            if (stack.IsEmpty() || stack.Peek() != '{') return false;
+                            stack.Pop();
+                            break;
+                        case ']':
+                            if (stack.IsEmpty() || stack.Peek() != '[') return false;
+                            stack.Pop();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+            }
+
+            if (stack.IsEmpty()) return true;
+
+            return false;
+        }
+    }
+}
