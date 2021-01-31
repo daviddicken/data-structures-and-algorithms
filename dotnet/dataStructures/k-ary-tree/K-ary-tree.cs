14066d8c5f4938d4a8de6efe8e572e51327af10a
daviddicken <davidfromseattle@gmail.com>
2021-01-30 23:39:04 -0800

    finished add method for k-ary-tree

---
+++ dotnet/dataStructures/k-ary-tree/K-ary-tree.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace k_ary_tree
+{
+    public class KTree<T>
+    {
+        public Node<T> Root { get; set; }
+        public int KidsAllowed { get; set; } // Children allowed per node
+
+        public KTree(int kidsAllowed) { KidsAllowed = kidsAllowed; }
+        public KTree(int kidsAllowed, T value) 
+        {
+            KidsAllowed = kidsAllowed;
+            Root.Value = value; 
+        }
+
+        // Methods
+        public void Add(T value)
+        {
+            if (Root == null) Root = new Node<T>(value);
+            var rand = new Random();
+            bool notPlaced = true;
+            Node<T> curentNode = Root;
+
+            while(notPlaced)
+                if(curentNode.Kids.Count < KidsAllowed)
+                {
+                    Node<T> newNode = new Node<T>(value);
+                    curentNode.Kids.Add(newNode);
+                    notPlaced = false;
+                }
+                else
+                {
+                    int index = rand.Next(curentNode.Kids.Count);
+                    curentNode = curentNode.Kids[index];
+                }
+        }
+    }
+}
---
+++ dotnet/dataStructures/k-ary-tree/Node.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace k_ary_tree
+{
+    class Node
+    {
+    }
+}
