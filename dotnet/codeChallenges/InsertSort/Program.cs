da5769ccba5cc438c480c88a39d7e9c125647744
daviddicken <davidfromseattle@gmail.com>
2021-02-15 22:56:57 -0800

    completed insert sort challenge

---
+++ dotnet/codeChallenges/InsertSort/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace InsertSort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] test = new int[] { 8, 4, 23, 42, 16, 15 };
+            //int[] sorted = InsertSort(test);
+
+            //foreach ( int num in sorted)
+            //{
+            //    Console.WriteLine(num);
+            //}
+            
+        }
+
+        /* InsertionSort(int[] arr)
+  
+    FOR i = 1 to arr.length
+    
+      int j <-- i - 1
+      int temp <-- arr[i]
+      
+      WHILE j >= 0 AND temp < arr[j]
+        arr[j + 1] <-- arr[j]
+        j <-- j - 1
+        
+      arr[j + 1] <-- temp
+        */
+        /// <summary>
+        /// InsertSort takes in a unsorted array of integers and iterates through the array moving the smaller values to the left until the array is sorted.
+        /// </summary>
+        /// <param name="arr">array of int's</param>
+        /// <returns>sorted array of int's</returns>
+        public static int [] InsertSort(int[] arr)
+        {
+            for(int i = 1; i < arr.Length; i++)
+            {
+                int j = i - 1;
+                int temp = arr[i];
+
+                while(j >= 0 && temp < arr[j])
+                {
+                    arr[j + 1] = arr[j];
+                    j -= 1;
+                }
+                arr[j + 1] = temp;
+            }
+            return arr;
+        }
+    }
+}
---
+++ dotnet/codeChallenges/InsertSortTest/InsertSortTest.cs
@@ -0,0 +1,49 @@
+using System;
+using Xunit;
+using InsertSort;
+
+namespace InsertSortTest
+{
+    public class InsertSortTest
+    {
+        [Fact]
+        public void HappyPath()
+        {
+            
+            int[] test = new int[] { 8, 4, 23, 42, 16, 15 };
+            int[] expected = new int[] { 4, 8, 15, 16, 23, 42 };
+
+            Assert.Equal(Program.InsertSort(test), expected);
+        }
+
+        [Fact]
+        public void ReverseSorted()
+        {
+
+            int[] test = new int[] { 20, 18, 12, 8, 5, -2 };
+            int[] expected = new int[] { -2,5,8,12,18,20};
+
+            Assert.Equal(Program.InsertSort(test), expected);
+        }
+
+        [Fact]
+        public void FewUniques()
+        {
+
+            int[] test = new int[] { 5,12,7,5,5,7};
+            int[] expected = new int[] { 5,5,5,7,7,12};
+
+            Assert.Equal(Program.InsertSort(test), expected);
+        }
+
+        [Fact]
+        public void NearlySorted()
+        {
+
+            int[] test = new int[] { 2, 3, 5, 7, 13, 11 };
+            int[] expected = new int[] { 2,3,5,7,11,13};
+            
+            Assert.Equal(Program.InsertSort(test), expected);
+        }
+    }
+}
