9e57013690580a13c1195306bfbaf3ed7bdf2135
daviddicken <davidfromseattle@gmail.com>
2021-02-22 22:44:21 -0800

    finished repeatword challenge

---
+++ dotnet/RepeatWords/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace RepeatWord
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      string input = "!23 Hello my name is not hello!!!!!***";
+      Console.WriteLine(RepeatWord(input));
+    }
+
+    //https://www.geeksforgeeks.org/removing-punctuations-given-string/
+    //by 29AjayKumar
+    public static string RepeatWord(string str)
+    {
+      HashSet<string> set = new HashSet<string>();
+
+      str = Regex.Replace(str, @"[^\w\d\s]","").ToLower();
+
+      string[] words = str.Split(' ');
+
+      foreach (string word in words)
+      {
+        if (set.Contains(word)) return word;
+        set.Add(word);
+      }
+      return "No Duplicates found";
+    }
+ 
+  }
+}
